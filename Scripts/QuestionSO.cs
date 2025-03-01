780c92a8bdead7ec6fbba0b9cd70014b44a3a307
MorriganDM <pedrovicthorcordeiro@gmail.com>
2025-02-28 21:31:44 -0300

    Update QuestionSO.cs
    Added several features

--- Scripts/QuestionSO.cs
+++ Scripts/QuestionSO.cs
@@ -7,4 +7,21 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)] 
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correct;
+
+    public string GetQuestion() //a função retornará um string
+    {
+        return question; //return ativa o retorno da função, e habilita a leitura da variável question.
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correct;
+    }
 }
