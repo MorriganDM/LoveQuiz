3fb2c294a836a262de8ebd8b79cf5fe22984f952
MorriganDM <pedrovicthorcordeiro@gmail.com>
2025-03-01 13:39:46 -0300

    Updated Time.cs

--- Scripts/Timer.cs
+++ Scripts/Timer.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    
+    public bool isAnsweringQuestion = false;
+    
+    float timerValue;
+
     void Update()
     {
-        
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime; //-= subtracts the left value to the right value
+
+        if(isAnsweringQuestion)
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+
+        Debug.Log(timerValue);
     }
 }
